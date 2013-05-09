c4ce26d6e1c15b855fa6aa02008a97305a3a6a92
Daniel Schweighoefer <daniel@netsteward.net>
2013-05-09 13:31:45 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+include FileUtils
+
+homedir = "/Users/#{ENV["USER"]}"
+path = File.dirname(File.expand_path(__FILE__)) + "/"
+
+local_confs = %w(gvimrc.local 
+		vimrc.local 
+		profile 
+		ackrc 
+		gitconfig)
+
+to_link = Hash.new
+local_confs.each do |f|
+  to_link.store path + f, homedir + "/." + f
+end
+to_link["#{path}com.googlecode.iterm2.plist"] = "#{homedir}/Library/Preferences/com.googlecode.iterm2.plist"
+
+to_link.each do |source,target|
+  if File.symlink? target
+    puts "#{source} already linked, nothing to do"
+  else 
+    mv target, target + ".old" if File.exist? target
+    ln_s source, target
+  end
+end
